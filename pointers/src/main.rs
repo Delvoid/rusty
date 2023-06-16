a5c8a42bda1e77ab955f2a6968a46f90b938dfc2
DaveHyperspace <dave@hyperspace.us>
2023-06-16 15:29:27 +0100

    concurrency

--- pointers/src/main.rs
+++ pointers/src/main.rs
@@ -1,8 +1,16 @@
+use crate::List::{Cons, Nil};
 use std::rc::Rc;
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
 fn main() {
     println!("Hello, world!");
     multiply();
-    q2()
+    q2();
+
+    let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 }
 
 fn multiply() {
