ed94430f41f6bf0b1b57f7ca31ab1d102a534b7c
DaveHyperspace <dave@hyperspace.us>
2023-06-28 16:16:58 +0100

    declarative macro

--- macros/src/main.rs
+++ macros/src/main.rs
@@ -1,3 +1,4 @@
+use art_macros::debug_print;
 macro_rules! gcd {
     ($a: expr, $b: expr) => {{
         let mut m = $b;
@@ -12,7 +13,23 @@ macro_rules! gcd {
         n
     }};
 }
+
+macro_rules! average {
+    ($(,)*) => {
+        0.0
+    };
+    ($($val: expr), + $(,)*) => {{
+        let count = 0usize $(+ {let _ = stringify!($val); 1})*;
+        let sum = 0.0 $(+ $val as f64)*;
+
+        sum / count as f64
+    }}
+}
+
+#[debug_print]
 fn main() {
     let test = gcd!(14, 17);
     println!("{}", test);
+    println!("{}", average!(1.0, 2.0, 3.0));
+    println!("{}", average!(1, 2, 3, 4, 5))
 }
