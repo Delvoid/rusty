2a8971cf84c83aad8496f5e27ea4972170239584
DaveHyperspace <dave@hyperspace.us>
2023-07-02 01:04:23 +0100

    fib

--- recursion/src/main.rs
+++ recursion/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("Factorial of 5 is {}", fact(5));
+    println!("Fibonacci of 5 is {}", fib(15));
 }
 
 fn fact(num: i32) -> i32 {
@@ -8,4 +9,16 @@ fn fact(num: i32) -> i32 {
     } else {
         num * fact(num - 1)
     }
+}
+
+fn fib(num: i32) -> i32 {
+    if num == 0 {
+        return 0;
+    }
+
+    if num == 1 {
+        return 1;
+    }
+
+    fib(num - 1) + fib(num - 2)
 }
