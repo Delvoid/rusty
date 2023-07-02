52fd06c58791ee55d87512a5403fe6a3f2d7a2e9
DaveHyperspace <dave@hyperspace.us>
2023-07-02 21:27:56 +0100

    palindrom

--- recursion/src/main.rs
+++ recursion/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
     println!("Factorial of 5 is {}", fact(5));
     println!("Fibonacci of 5 is {}", fib(15));
+
+    let string = "racecar";
+    println!("{:?}", palindrome(string));
+
+    let string = "hello";
+    println!("{:?}", palindrome(string));
 }
 
 fn fact(num: i32) -> i32 {
@@ -21,4 +27,16 @@ fn fib(num: i32) -> i32 {
     }
 
     fib(num - 1) + fib(num - 2)
+}
+
+fn palindrome(word: &str) -> bool {
+    if word.len() <= 1 {
+        return true;
+    }
+
+    if word.chars().next() == word.chars().last() {
+        palindrome(&word[1..word.len() - 1])
+    } else {
+        false
+    }
 }
