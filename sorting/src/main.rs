2fc6d771e38e63991b7a33a112f3491493d4d76e
DaveHyperspace <dave@hyperspace.us>
2023-07-03 17:46:14 +0100

    merge sort

--- sorting/src/main.rs
+++ sorting/src/main.rs
@@ -12,7 +12,12 @@ fn main() {
 
     println!("Before: {:?}", arr);
     bubble_sort(&mut arr);
-    println!("Bubble After: {:?}", arr)
+    println!("Bubble After: {:?}", arr);
+
+    let mut arr: Vec<i32> = vec![10, 5, 3, 2, 4, 6, 9, 7, 8, 1];
+    merge_sort(&mut arr);
+
+    println!("Merge After: {:?}", arr);
 }
 
 fn selection_sort(arr: &mut [i32]) -> Vec<i32> {
@@ -51,4 +56,36 @@ fn bubble_sort(arr: &mut [i32]) -> Vec<i32> {
         }
     }
     arr.to_vec()
+}
+
+fn merge_sort(arr: &mut [i32]) -> Vec<i32> {
+    let len = arr.len();
+
+    if len > 1 {
+        let mid = arr.len() / 2;
+        merge_sort(&mut arr[..mid]); // left
+        merge_sort(&mut arr[mid..]); // right
+
+        merge(arr, mid)
+    }
+
+    arr.to_vec()
+}
+
+fn merge(arr: &mut [i32], mid: usize) {
+    let left = arr[..mid].to_vec();
+    let right = arr[mid..].to_vec();
+
+    let mut l = 0;
+    let mut r = 0;
+
+    for val in arr {
+        if r == right.len() || (l < left.len() && left[l] < right[r]) {
+            *val = left[l];
+            l += 1;
+        } else {
+            *val = right[r];
+            r += 1;
+        }
+    }
 }
