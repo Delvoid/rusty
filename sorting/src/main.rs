07f857358b89cd890f89b4f0ae1d98445fe64c5b
DaveHyperspace <dave@hyperspace.us>
2023-07-03 18:24:33 +0100

    quick sort

--- sorting/src/main.rs
+++ sorting/src/main.rs
@@ -18,6 +18,10 @@ fn main() {
     merge_sort(&mut arr);
 
     println!("Merge After: {:?}", arr);
+
+    let mut arr: Vec<i32> = vec![10, 5, 3, 2, 4, 6, 9, 7, 8, 1];
+    quick_sort(&mut arr);
+    println!("Quick After: {:?}", arr);
 }
 
 fn selection_sort(arr: &mut [i32]) -> Vec<i32> {
@@ -88,4 +92,31 @@ fn merge(arr: &mut [i32], mid: usize) {
             r += 1;
         }
     }
+}
+
+fn quick_sort<T: Ord>(arr: &mut [T]) {
+    let len = arr.len();
+    if len < 2 {
+        return;
+    }
+
+    let pivot_index = partition(arr);
+    let (left, right) = arr.split_at_mut(pivot_index);
+    quick_sort(left);
+    quick_sort(&mut right[1..]);
+}
+
+fn partition<T: Ord>(arr: &mut [T]) -> usize {
+    let len = arr.len();
+    let pivot_index = len / 2;
+    arr.swap(pivot_index, len - 1);
+    let mut i = 0;
+    for j in 0..len - 1 {
+        if arr[j] <= arr[len - 1] {
+            arr.swap(i, j);
+            i += 1;
+        }
+    }
+    arr.swap(i, len - 1);
+    i
 }
