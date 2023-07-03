0ac6dbd544a855ef07886f95f39a7a8efd77ad54
DaveHyperspace <dave@hyperspace.us>
2023-07-03 17:04:18 +0100

    bubble sort

--- sorting/src/main.rs
+++ sorting/src/main.rs
@@ -3,10 +3,16 @@ fn main() {
 
     let mut arr = [1, 5, 3, 2, 4];
 
-    println!("Before: {:?}", arr);
+    println!("Selection sortBefore: {:?}", arr);
     selection_sort(&mut arr);
 
     println!("After: {:?}", arr);
+
+    let mut arr: Vec<i32> = vec![1, 5, 3, 2, 4];
+
+    println!("Before: {:?}", arr);
+    bubble_sort(&mut arr);
+    println!("Bubble After: {:?}", arr)
 }
 
 fn selection_sort(arr: &mut [i32]) -> Vec<i32> {
@@ -23,6 +29,25 @@ fn selection_sort(arr: &mut [i32]) -> Vec<i32> {
 
         if min != i {
             arr.swap(i, min);
+        }
+    }
+    arr.to_vec()
+}
+
+fn bubble_sort(arr: &mut [i32]) -> Vec<i32> {
+    let len = arr.len();
+    let mut sorted: bool;
+
+    for i in 0..len {
+        sorted = true;
+        for j in (i + 1)..len {
+            if arr[j] < arr[i] {
+                arr.swap(i, j);
+                sorted = false;
+            }
+        }
+        if sorted {
+            break;
         }
     }
     arr.to_vec()
